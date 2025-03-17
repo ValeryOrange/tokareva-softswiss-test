7254f2fc55457a7b799426d0a701c1ca25e440ae
Valerie Tokareva <tkrv.valery@gmail.com>
2025-03-17 14:10:06 +0400

    finished base style settings and added favicon

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,21 @@
+const path = require("path");
+
 module.exports = {
-    style: {
-      sass: {
-        loaderOptions: {
-          additionalData: `@use "src/styles/breakpoints.scss" as breakpoints;`
+    webpack: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@styles": path.resolve(__dirname, "src/styles"),
+            "@components": path.resolve(__dirname, "src/components"),
         },
-      },
+    },
+    style: {
+        sass: {
+            loaderOptions: {
+                additionalData: `
+                    @use "src/styles/breakpoints.scss" as *;
+                    @use "src/styles/variables.scss" as *;
+                `
+            },
+        },
     },
 };
