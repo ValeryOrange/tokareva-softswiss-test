9e6426a051205718e6888a17c1eb4e79784f3c6d
Valerie Tokareva <tkrv.valery@gmail.com>
2025-03-19 18:49:13 +0400

    removed suspense from cards component

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,22 +1,27 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import Title from '@components/Title';
 import './cards.scss';
 
 const Card = React.lazy(() => import('@components/Card'));
 
+/**
+ * CardsList component
+ *
+ * @param {Object} props - Component props
+ * @param {Array} [props.cards=[]] - List of cards to display, defaults to an empty array
+ * @param {string} props.title - Title of the cards list
+ */
 const CardsList = ({ cards = [], title }) => {
     return (
         <div className="cards">
             <Title size="h3">{title}</Title>
             {cards.length ? (
                 <ul className="cards__list">
-                    <Suspense fallback={<div>Loading cards...</div>}>
-                        {cards.map((card) => (
-                            <li key={card.id} className="cards__item">
-                                <Card {...card} />
-                            </li>
-                        ))}
-                    </Suspense>
+                    {cards.map((card) => (
+                        <li key={card.id} className="cards__item">
+                            <Card {...card} />
+                        </li>
+                    ))}
                 </ul>
             ) : (
                 'No cards available'
